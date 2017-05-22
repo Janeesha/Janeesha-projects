0b0567698d3360ba3e85af6025d3267eac00ea8f
unknown <hima.ratikanti@gmail.com>
2017-05-22 09:57:06 +0530

    node

---
+++ nodejsproject/server1.js
@@ -0,0 +1,11 @@
+
+var http=require("http");
+var server=http.createServer(function(request,response)
+{
+	response.writeHead(200,{'content-type':'text'});
+	response.end("sucess");
+});
+
+server.listen(3000,function(){
+	console.log("server is listening at 3000");
+})
